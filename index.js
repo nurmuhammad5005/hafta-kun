a2461f1e8f89ccad78d86809bc3af59a9973f873
Nurmuhammad Fazlillaev <nurmuhammadfazlillaev@gmail.com>
2025-02-06 16:01:50 +0500

    hafta kun

---
+++ index.js
@@ -0,0 +1,40 @@
+let kun = prompt("hafta kuni kirit alo"); // "7" === 7
+
+
+
+
+if (kun === "7") {
+    alert("yakshanba")
+}
+
+
+if (kun === "6") {
+    alert("shanba")
+}
+
+
+if (kun === "5") {
+    alert("juma")
+}
+
+if (kun === "4") {
+    alert("payshanba")
+}
+
+if (kun === "3") {
+    alert("chorshanba")
+}
+
+if (kun === "2") {
+    alert("seshanba")
+}
+
+if (kun === "1") {
+    alert("duwanba")
+}
+  
+
+
+if( kun > 7)
+    {alert("xato")}
+
